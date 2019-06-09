6dc5e1abfa52fbedd42f4b39a7642c2f2c1a6bc2
Leonel Toledo <leonel.toledo95@gmail.com>
2019-06-09 15:50:00 -0300

    Subo Tests Unitarios

---
+++ TP3/Toledo.Leonel.2D.TP3/TestUnitario/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUnitario")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestUnitario")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a3d295d8-63c8-4096-b801-11139ec98b80")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TP3/Toledo.Leonel.2D.TP3/TestUnitario/Test.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Clases_Abstractas;
+using Clases_Instanciables;
+using Excepciones;
+
+namespace TestUnitario
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void NacionalidadInvalidaExceptionTest()
+        {
+            //Arrange
+            Alumno alumno;
+
+            //Act
+            try
+            {
+                alumno = new Alumno(1, "Leonel", "Toledo", "90000000", Persona.ENacionalidad.Argentino, Universidad.EClases.Legislacion);
+            }
+
+            //Assert
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(NacionalidadInvalidaException));
+            }
+        }
+
+        [TestMethod]
+        public void AlumnoRepetidoExceptionTest()
+        {
+            //Arrange
+            Alumno alumno1 = new Alumno(1, "Leonel", "Toledo", "34000000", Persona.ENacionalidad.Argentino, Universidad.EClases.Legislacion);
+            Alumno alumno2 = new Alumno(1, "Juan", "Perez", "34000000", Persona.ENacionalidad.Argentino, Universidad.EClases.Legislacion);
+            Universidad universidad = new Universidad();
+
+            //Act
+            try
+            {
+                universidad += alumno1;
+                universidad += alumno2;
+            }
+
+            //Assert
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(AlumnoRepetidoException));
+            }
+        }
+
+        [TestMethod]
+        public void DniTest()
+        {
+            //Arrange
+            int dni = 38889128;
+            Alumno aux = new Alumno(1, "Leonel", "Toledo", dni.ToString(), Persona.ENacionalidad.Argentino, Universidad.EClases.Legislacion);
+
+            //Act
+            int resultado = aux.DNI;
+
+            //Assert
+            Assert.AreEqual(dni, resultado);
+        }
+
+        [TestMethod]
+        public void NewProfesorTest()
+        {
+            //Arrange
+            Profesor profe = new Profesor(1, "Juan", "Perez", "30000000", Persona.ENacionalidad.Argentino);
+
+            //Assert
+            Assert.IsNotNull(profe.DNI);
+            Assert.IsNotNull(profe.Nombre);
+            Assert.IsNotNull(profe.Apellido);
+            Assert.IsNotNull(profe.Nacionalidad);
+        }
+    }
+}
